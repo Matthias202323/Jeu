58f683d1e710fdad05be639c8853af5129458908
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-04-10 08:41:52 +0200

    Ajoutez des fichiers projet.

---
+++ Combat.cs
@@ -0,0 +1,14 @@
+﻿
+using Video;
+
+Personnage A = new Personnage();
+Personnage B = new Personnage();
+A.NamePerso();
+B.NamePerso();
+A.Attack(B);
+B.Attack(A);
+A.Attack(B);
+B.Attack(A); 
+while(A.IsAlive()) { B.Attack(A); }
+if (!A.IsAlive()) { Console.WriteLine(B.name + " a gagné"); }
+
---
+++ Personnage.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace Video
+{
+    internal class Personnage
+    {
+        public int pv = 20;
+        public int force = 18;
+        public int défense = 14;
+
+        public string name = "";
+     
+
+        public string NamePerso()
+        {
+            Console.WriteLine("Enterez un nom");
+            name = Console.ReadLine();
+            return name;
+
+        }
+        public bool IsAlive()
+        {
+            if (pv > 0)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Le personnage " + name + " est mort");
+                return false;
+            }
+        }
+
+        public void Attack(Personnage A)
+        {
+            A.pv = (A.pv) - (force - A.défense);
+            Console.WriteLine(A.name  +" Ponts de vie="+ A.pv);
+        }
+    }
+
+   
+}
+
